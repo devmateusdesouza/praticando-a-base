2c6b1dc0de43709ed9dc8f6a500cf7b5bc112357
Mateus de Souza <devmateusdesouza@users.noreply.github.com>
2023-06-27 11:31:37 -0400

    post tower defense
    leia o README.

---
+++ tower-defense/README.md
@@ -0,0 +1,8 @@
+TOWER DEFENSE - Reprodução da aula do Chris Courses - https://youtu.be/C4_iRLlPNFc
+
+Uma forma que foi muito importante no meu aprendizado para fixar os conhecimentos, foi treinar programando games.
+
+O principal objetivo desse projeto foi aprender mais sobre a manipulação do CANVA usando Javascript.
+
+O projeto está incompleto, pois já cumpriu o objetivo que era fixar o que queria aprender. Mas pretendo em um futuro não tão distante finalizá-lo.
+
---
+++ tower-defense/js/index.js
@@ -0,0 +1,122 @@
+const waypoints = [
+    {
+      x: -124.21331566744,
+      y: 475.322954620735
+    },
+    {
+      x: 276.581649552832,
+      y: 472.01059953627
+    },
+    {
+      x: 276.581649552832,
+      y: 157.33686651209
+    },
+    {
+      x: 735.342828751242,
+      y: 158.993044054323
+    },
+    {
+      x: 735.342828751242,
+      y: 405.763497846969
+    },
+    {
+      x: 606.160980457105,
+      y: 407.419675389202
+    },
+    {
+      x: 606.160980457105,
+      y: 669.095727061941
+    },
+    {
+      x: 1046.70420669096,
+      y: 665.783371977476
+    },
+    {
+      x: 1050.01656177542,
+      y: 288.17489234846
+    },
+    {
+      x: 1407.75091089765,
+      y: 286.518714806227
+    }
+  ]
+  
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1280
+canvas.height= 768
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.onload = () => {
+    animate()
+}
+image.src = 'img/gameMap.png'
+
+class Enemy {
+    constructor({ position = { x:0, y:0 } }){
+        this.position= position
+        this.width = 100
+        this.height = 100
+        this.waypointIndex = 0
+        this.center = {
+            x: this.position.x + this.width/2,
+            y: this.position.y + this.height/2
+        }
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(){
+        this.draw()
+        
+        const waypoint = waypoints[this.waypointIndex]
+        const yDistance = waypoint.y - this.center.y
+        const xDistance = waypoint.x - this.center.x
+        const angle = Math.atan2(yDistance, xDistance)
+        this.position.x += Math.cos(angle)
+        this.position.y += Math.sin(angle)
+        this.center = {
+            x: this.position.x + this.width/2,
+            y: this.position.y + this.height/2
+        }
+
+
+        if( 
+            Math.round(this.center.x) === Math.round(waypoint.x) && 
+            Math.round(this.center.y) === Math.round(waypoint.y) &&
+            this.waypointIndex < waypoints.length-1
+            ){
+            this.waypointIndex++
+        }
+
+    }
+
+}
+
+const enemies = []
+for( i = 1; i < 10 ; i++){
+    const xOffset = i *150
+    enemies.push(
+        new Enemy({
+            position : { x: waypoints[0].x - xOffset , y: waypoints[0].y }
+        }) 
+    )
+    console.log(waypoints[0].x - xOffset)
+}
+
+function animate (){
+    requestAnimationFrame(animate)
+
+    c.drawImage(image, 0, 0)
+    enemies.forEach((enemy)=>{
+        enemy.update()
+    })
+}
+
