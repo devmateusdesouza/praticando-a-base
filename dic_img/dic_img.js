e1d9ca3de7d1b22363a915c3c65b43bff16fdd41
Mateus de Souza <devmateusdesouza@users.noreply.github.com>
2023-06-27 11:06:44 -0400

    postando projeto antigo
    Esse projeto eu fiz quando estava aprendendo as bases do Javascript. E resolvi praticar fazendo um pequeno sistema para praticar inglês. Pretendo fazer uma versão 2.0 corrigindo erros que cometi e melhorando o design.

---
+++ dic_img/dic_img.js
@@ -0,0 +1,30 @@
+        
+var i=1
+function verificar(){
+    var txtimg = document.getElementById('img').src
+    console.log(txtimg)
+    var txtimgs = txtimg.slice(70,-4) //no servidor podem ser outros números, depende do taamanho do caminho
+    console.log(txtimgs)
+    var palavra = document.getElementById('input-text').value
+
+    if(txtimgs==palavra){
+        document.getElementById("text_confirmation").style.color = 'green'
+        document.getElementById("text_confirmation").innerHTML='  Acertou'
+
+        var names = ['img/officer.jpg' ,'img/dentist.jpg' , 'img/doctor.jpg', 'img/fireman.jpg' , 'img/teacher.jpg']
+
+        function newimage(){
+            document.getElementById('img').src = names[i]
+            document.getElementById('input-text').value = ''
+            document.getElementById("text_confirmation").innerHTML = ''
+            i= i+1
+        }
+        setTimeout(newimage, 1000)
+
+    }else{
+        document.getElementById("text_confirmation").style.color = 'red'
+        document.getElementById("text_confirmation").innerHTML='Errou'
+    }
+    
+}
+
