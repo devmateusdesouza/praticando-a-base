06024c5376340b87ccaa69e82428d4954c6b887c
Mateus de Souza <96146656+devmateusdesouza@users.noreply.github.com>
2023-06-28 10:11:51 -0400

    Update dic_img.js

--- dic-img/dic_img.js
+++ dic-img/dic_img.js
@@ -3,7 +3,8 @@ var i=1
 function verificar(){
     var txtimg = document.getElementById('img').src
     console.log(txtimg)
-    var txtimgs = txtimg.slice(70,-4) //no servidor podem ser outros números, depende do tamanho do caminho
+    var txtimgs = txtimg.slice(66,-4) //no servidor podem ser outros números, depende do tamanho do caminho
+        //no computador é (70,-4) no github é (66, -4)
     console.log(txtimgs)
     var palavra = document.getElementById('input-text').value
 
